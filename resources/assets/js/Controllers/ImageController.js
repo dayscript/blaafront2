0fb1a2e43721c674e7fcedcfe8790fafcdae9d4b
aacevedoday <aacevedo@dayscript.com>
2016-03-10 15:19:37 -0500

    url

--- resources/assets/js/Controllers/ImageController.js
+++ resources/assets/js/Controllers/ImageController.js
@@ -1,5 +1,5 @@
 App.controller('ImageController',function($scope,$http,$timeout,$q,$log){
-  $http.get('http://blaa.local/archivos/imagen/artistas').success(function(data,status,headers,config){
+  $http.get('http://blaa.demodayscript/archivos/imagen/artistas').success(function(data,status,headers,config){
     $scope.images = data.nodes
     console.log($scope.images);
   })
