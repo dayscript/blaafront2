1a7c4477f004d384802a238e1b7b0347ed960bd6
aacevedoday <aacevedo@dayscript.com>
2016-03-31 08:14:49 -0500

    imagenes opus

--- resources/assets/js/Controllers/ImageController.js
+++ resources/assets/js/Controllers/ImageController.js
@@ -1,13 +1,14 @@
 App.controller('ImageController1',function($scope,$http,$timeout,$q,$log,SERVER){
-  $http.get('http://blaa.dev/musica/conciertos/img/json').success(function(data,status,headers,config){
+  $http.get('http://blaafront2.demodayscript.com/musica/conciertos/img/json').success(function(data,status,headers,config){
     $scope.images = data.nodes
     console.log($scope.images);
   })
 });
 
 App.controller('ImageController2',function($scope,$http,$timeout,$q,$log,SERVER){
-  $http.get('http://blaa.dev/musica/conciertos/img/json').success(function(data,status,headers,config){
+  $http.get('http://blaafront2.demodayscript.com/musica/conciertos/img/json').success(function(data,status,headers,config){
     $scope.images = data.nodes
     console.log($scope.images);
   })
 });
+ 
