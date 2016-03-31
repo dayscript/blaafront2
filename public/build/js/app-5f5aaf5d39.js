6a6fac9fb0b2f540b1c16c40d0332dc751b136d4
aacevedoday <aacevedo@dayscript.com>
2016-03-31 08:18:27 -0500

    Imagenes

--- public/build/js/app-5f5aaf5d39.js
+++ public/build/js/app-5f5aaf5d39.js
@@ -73,18 +73,19 @@ App.controller('SearchController', function($scope,$http,$timeout,$q,$log,$rootS
 });
 
 App.controller('ImageController1',function($scope,$http,$timeout,$q,$log,SERVER){
-  $http.get('http://blaa.dev/musica/conciertos/img/json').success(function(data,status,headers,config){
+  $http.get('http://blaafront2.demodayscript.com/musica/conciertos/img/json').success(function(data,status,headers,config){
     $scope.images = data.nodes
     console.log($scope.images);
   })
 });
 
 App.controller('ImageController2',function($scope,$http,$timeout,$q,$log,SERVER){
-  $http.get('http://blaa.dev/musica/conciertos/img/json').success(function(data,status,headers,config){
+  $http.get('http://blaafront2.demodayscript.com/musica/conciertos/img/json').success(function(data,status,headers,config){
     $scope.images = data.nodes
     console.log($scope.images);
   })
 });
+ 
 
 App.factory('AutorsService', function($http) {
     var Autors = {
