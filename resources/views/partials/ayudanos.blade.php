660577c3ea3ff2ee88fab55a82081a1ac4ca3bfb
aacevedoday <aacevedo@dayscript.com>
2016-09-09 16:47:29 -0500

    update

--- resources/views/partials/ayudanos.blade.php
+++ resources/views/partials/ayudanos.blade.php
@@ -8,5 +8,5 @@
 
 </div>
 <div class="form-help columns medium-12">
-  <label><a href="http://blaa.demodayscript.com/content/download/all/all/all/all/all/all/conciertos.xls">Descargar toda la base de datos</a></label>
+  <label><a target="_blank" href="http://blaa.demodayscript.com/content/download/all/all/all/all/all/all/conciertos.xls">Descargar toda la base de datos</a></label>
 </div>
