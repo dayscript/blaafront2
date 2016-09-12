32bb1b67d890150242296d4a544ebdc74bed0109
aacevedoday <aacevedo@dayscript.com>
2016-09-12 17:57:03 -0500

    update

--- resources/assets/js/Controllers/ImageController.js
+++ resources/assets/js/Controllers/ImageController.js
@@ -2,7 +2,7 @@ App.controller('ImageController',function($scope,$http,$timeout,$q,$log,SERVER,S
   console.log(SERVERFRONT.domain)
   //$http.get(SERVERFRONT.domain+'/musica/conciertos/img/json').success(function(data,status,headers,config){
   $http.get(SERVERFRONT.domain+'/musica/conciertos/img/json').success(function(data,status,headers,config){
-    console.log(data);
+    //console.log(data);
     $scope.images = data
   })
 });
