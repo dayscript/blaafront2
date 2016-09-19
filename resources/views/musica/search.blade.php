f801cd605fedaf0754284f3cfc6aa2364196e7b8
jcardenas2015 <jcardenas@dayscript.com>
2016-09-19 15:07:37 -0500

    Ajustes diseño

--- resources/views/musica/search.blade.php
+++ resources/views/musica/search.blade.php
@@ -13,8 +13,8 @@
                 <label class="medium-12">{{$itemSearch}}</label>
                 <label class="medium-12 red"><strong>{{$nodes->view->count}}</strong> Resultados</label>
             </div>
-            <div class="medium-4 columns number-items">
-                <span>Resultados por pagina </span>
+            <div class="medium-3 columns number-items">
+                <span>Resultados</span>
                 <select id="number-items" onchange=" this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value); ">
                   <option value="musica/opus/resultados/0?items=10">10</option>
                   <option value="musica/opus/resultados/0?items=20">20</option>
@@ -23,16 +23,17 @@
                   <option value="musica/opus/resultados/0?items=50">50</option>
                 </select>
             </div>
-            <div class="medium-2 columns">
+            <div class="medium-3 columns">
               <div class="row order-by">
-                  <div class="medium-6 columns">
-                      <label>Ordenar: </label>
+                  <div class="medium-4 columns text-left">
+                      <label>Ordenar:   </label>
                   </div>
-               <div class="medium-6 columns">
+               <div class="medium-4 columns text-right">
                    <a href="{{Request::path().$Params->str_params}}elim=crono" id="order-by"></a>
-                   <a href="{{Request::path().$Params->str_params}}elim=orden" id="order-by"></a>
-
                </div>
+                  <div class="medium-4 columns text-right">
+                      <a href="{{Request::path().$Params->str_params}}elim=crono" id="crono-by"></a>
+                  </div>
               </div>
             </div>
             <div class="medium-3 columns number-page">
