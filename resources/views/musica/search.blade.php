dbf2e088fc9caf988e07ae935eaa919222183136
aacevedoday <aacevedo@dayscript.com>
2016-10-26 19:30:33 -0500

    odigo

--- resources/views/musica/search.blade.php
+++ resources/views/musica/search.blade.php
@@ -8,47 +8,7 @@
     @include('partials.ayudanos')
     </div>
     <div class="columns medium-8 results-view">
-        <div class="paginator row columns">
-            <div class="medium-3 items-search columns">
-                <label class="medium-12">{{$itemSearch}}</label>
-                <label class="medium-12 red"><strong>{{$nodes->view->count}}</strong> Resultados</label>
-            </div>
-            <div class="medium-3 columns number-items">
-                <span>Resultados</span>
-                <select id="number-items" onchange=" this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value); ">
-                  <option value="musica/opus/resultados/0?items=10">10</option>
-                  <option value="musica/opus/resultados/0?items=20">20</option>
-                  <option value="musica/opus/resultados/0?items=30">30</option>
-                  <option value="musica/opus/resultados/0?items=40">40</option>
-                  <option value="musica/opus/resultados/0?items=50">50</option>
-                </select>
-            </div>
-            <div class="medium-3 columns">
-              <div class="row order-by">
-                  <div class="medium-4 columns text-left">
-                      <label>Ordenar:   </label>
-                  </div>
-               <div class="medium-4 columns text-right">
-                   <a href="{{Request::path().$Params->str_params}}elim=crono" id="order-by"></a>
-               </div>
-                  <div class="medium-4 columns text-right">
-                      <a href="{{Request::path().$Params->str_params}}elim=orden" id="crono-by"></a>
-                  </div>
-              </div>
-            </div>
-            <div class="medium-3 columns number-page">
-                <ul class="pagination" role="menubar" aria-label="Pagination">
-                  <li class="arrow unavailable" aria-disabled="true"><a href="musica/opus/resultados/{{ $nodes->view->page-1 }}">&laquo; </a></li>
-                  @for( $i = $nodes->view->page+1 ; $i <= $nodes->view->pages; $i++ )
-                      @if( $i <= $nodes->view->page+4)
-                            <li class="{{ ($nodes->view->page+1 == $i ) ? 'current':'' }}"><a href="/musica/opus/resultados/{{$i-1}}">{{$i}}</a></li>
-                      @endif
-                  @endfor
-                  <li class="arrow"><a href="musica/opus/resultados/{{ $nodes->view->page+1 }}"> &raquo;</a></li>
-                </ul>
-              </div>
-
-        </div>
+      @include('partials.paginator')
         <div class="results medium-12 columns">
         @foreach($nodes->nodes as $node)
             <div class="row">
@@ -78,5 +38,8 @@
             <hr>
         @endforeach
         </div>
+
+        @include('partials.paginator')
+
     </div>
 @endsection
---
+++ resources/views/partials/paginator.blade.php
@@ -0,0 +1,40 @@
+<div class="paginator row columns">
+    <div class="medium-3 items-search columns">
+        <label class="medium-12">{{$itemSearch}}</label>
+        <label class="medium-12 red"><strong>{{$nodes->view->count}}</strong> Resultados</label>
+    </div>
+    <div class="medium-3 columns number-items">
+        <span>Resultados</span>
+        <select id="number-items" onchange=" this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value); ">
+          <option value="musica/opus/resultados/0?items=10">10</option>
+          <option value="musica/opus/resultados/0?items=20">20</option>
+          <option value="musica/opus/resultados/0?items=30">30</option>
+          <option value="musica/opus/resultados/0?items=40">40</option>
+          <option value="musica/opus/resultados/0?items=50">50</option>
+        </select>
+    </div>
+    <div class="medium-3 columns">
+      <div class="row order-by">
+          <div class="medium-4 columns text-left">
+              <label>Ordenar:   </label>
+          </div>
+       <div class="medium-4 columns text-right">
+           <a href="{{Request::path().$Params->str_params}}elim=crono" id="order-by"></a>
+       </div>
+          <div class="medium-4 columns text-right">
+              <a href="{{Request::path().$Params->str_params}}elim=orden" id="crono-by"></a>
+          </div>
+      </div>
+    </div>
+    <div class="medium-3 columns number-page">
+        <ul class="pagination" role="menubar" aria-label="Pagination">
+          <li class="arrow unavailable" aria-disabled="true"><a href="musica/opus/resultados/{{ $nodes->view->page-1 }}">&laquo; </a></li>
+          @for( $i = $nodes->view->page+1 ; $i <= $nodes->view->pages; $i++ )
+              @if( $i <= $nodes->view->page+4)
+                    <li class="{{ ($nodes->view->page+1 == $i ) ? 'current':'' }}"><a href="/musica/opus/resultados/{{$i-1}}">{{$i}}</a></li>
+              @endif
+          @endfor
+          <li class="arrow"><a href="musica/opus/resultados/{{ $nodes->view->page+1 }}"> &raquo;</a></li>
+        </ul>
+      </div>
+</div>
